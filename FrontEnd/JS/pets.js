fd93b30ce16e039b2075e5d1b051e3f8c32d09d5
alvaroescram <escotoalvaro96@gmail.com>
2021-11-13 18:03:53 -0600

    update Pets.js

--- FrontEnd/JS/pets.js
+++ FrontEnd/JS/pets.js
@@ -6,7 +6,7 @@ class PetsException{
 }
 
 class Pets{
-    constructor(){
+    constructor(id,ownerId,status,location,breed,age,img_links,mainPic,animaltype){
        this._id;
        this._ownerId;
        this._status;
@@ -16,6 +16,45 @@ class Pets{
        this._img_links;
        this._mainPicLink;
        this._animaltype;
+    }
+    set id(val){
+        return new PetsException("Can't change Pets id");
+    }
+    get id(){
+        return this._id;
+    }
+    set ownerId(val){
+        return new PetsException("Can't change owner Id");
+    }
+    get ownerId(){
+        return this._ownerId;
+    }
+    set status(val){
+        this._status = val;
+    }
+    get status(){
+        return this._status;
+    }
+    set location(val){
+        this._location = val;
+    }
+    get location(){
+        return this._location;
+    }
+    set breed(val){
+        this._breed = val;
+    }
+    get breed(){
+        return this_breed;
+    }
+    set age(val){
+        this._age = val;
+    }
+    get age(){
+        return this._age;
+    }
+    set img_links(val){
+        this._img_links = val ;
     }
 
 }
