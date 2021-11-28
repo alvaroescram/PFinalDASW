73e93a49fffa68b5fcab0f0b96e4fddd2bc3ae2c
Santiago0192 <94256359+Santiago0192@users.noreply.github.com>
2021-11-27 18:11:58 -0600

    Update index.js

--- FrontEnd/JS/index.js
+++ FrontEnd/JS/index.js
@@ -1,20 +1,20 @@
 'use strict';
 
 function addMenu(){
-    
     let menu = document.getElementById('header');
     menu.innerHTML = setmenu();
-
 }
+
 function addPetcard(id,photoLink,name,description,ownerId,status,sharelink){
     let petCards = document.getElementById("petsCards");
     let card = PetCard(id,photoLink,name,description,ownerId,status,sharelink);
-    petCards.innerHTML = petCards.innerHTML+ card;
-    console.log(petCards);
+    petCards.innerHTML = petCards.innerHTML + card;
+}
 
+function removePetcard(id){
+    let petCard = document.getElementById(id+"card")
+    petCard.remove()
 }
 addMenu();
 addPetcard(1,'https://storage.googleapis.com/mvsnoticias/2021/10/31fdef09-pexels-blue-bird-7210644-800x1200.jpg',"Tommy","Good Boy",2,"Adopted","facebook.com");
-
-
-// PetCard()
+//removePetcard(1)
